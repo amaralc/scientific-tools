05feb954565def3ff0f3e0ad3b4d9f085e02a82a
amaralc <amaral.calil@gmail.com>
2022-11-29 22:12:59 -0300

    config: add basic tailwind config from template

--- apps/mkt-mfe-sampling-tree-shell/tailwind.config.js
+++ apps/mkt-mfe-sampling-tree-shell/tailwind.config.js
@@ -19,44 +19,45 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media'
   theme: {
-    fontFamily: {
-      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '2rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['2rem', { lineHeight: '2.5rem' }],
+      '4xl': ['2.5rem', { lineHeight: '3.5rem' }],
+      '5xl': ['3rem', { lineHeight: '3.5rem' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1.1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
     },
-
     extend: {
-      colors: {
-        primary: '#228be6',
-        // 'primary-dark': darken('#228be6', 0.1),
-        // 'primary-light': lighen('#228be6', 0.1),
+      borderRadius: {
+        '4xl': '2rem',
       },
-      screens: {
-        xs: { min: '0px', max: '639px' },
-        ...defaultTheme.screens,
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Lexend', ...defaultTheme.fontFamily.sans],
       },
-
-      containerQuery: {
-        xs: '475px',
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
-      },
-      containerType: {
-        size: 'size',
+      maxWidth: {
+        '2xl': '40rem',
       },
     },
   },
+  plugins: [require('@tailwindcss/forms')],
   variants: {
     extend: {},
   },
-  plugins: [
-    // require('@tailwindcss/line-clamp'),
-    /**
-     * https://morioh.com/p/cb1b7af5f6d8
-     * https://stackoverflow.com/questions/12251750/can-media-queries-resize-based-on-a-div-element-instead-of-the-screen
-     * https://github.com/w3c/csswg-drafts/issues/5796
-     */
-    // require('tailwindcss-container-query'),
-  ],
+  // plugins: [
+  //   // require('@tailwindcss/line-clamp'),
+  //   /**
+  //    * https://morioh.com/p/cb1b7af5f6d8
+  //    * https://stackoverflow.com/questions/12251750/can-media-queries-resize-based-on-a-div-element-instead-of-the-screen
+  //    * https://github.com/w3c/csswg-drafts/issues/5796
+  //    */
+  //   // require('tailwindcss-container-query'),
+  // ],
 };
