badfee5c278631c5187e72f165fcddca68bf7e6b
amaralc <amaral.calil@gmail.com>
2022-12-13 19:40:58 -0300

    feat: adjust footer styles

--- apps/product-mfe-sampling-tree/pages/plans/new.tsx
+++ apps/product-mfe-sampling-tree/pages/plans/new.tsx
@@ -187,19 +187,24 @@ export default function Example() {
                       </td>
                     </tr>
                   ))}
+                  <tr>
+                    <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-500"></td>
+                    <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-500"></td>
+                    <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-500"></td>
+                    <td className="whitespace-nowrap flex justify-end px-3 py-4 text-sm text-gray-500">
+                      <button
+                        type="button"
+                        className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                      >
+                        Add factor
+                      </button>
+                    </td>
+                  </tr>
                 </tbody>
               </table>
             </div>
           </div>
         </div>
-      </div>
-      <div className="my-4 flex justify-end">
-        <button
-          type="button"
-          className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-        >
-          Add factor
-        </button>
       </div>
     </div>
   );
